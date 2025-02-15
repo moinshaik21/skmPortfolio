7b3752001b1c8f1a6e28e86d4d786398b8298c6a
Khaja <mohinsk1111@gmail.com>
2025-02-15 18:20:37 +0530

    Techonologies issue

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -8,9 +8,11 @@ import { technologies } from "../constants";
 
 const Tech = () => {
   return (
-    <div className='flex flex-row flex-wrap justify-center gap-10'>
+    <div className=' flex flex-wrap justify-center items-center gap-10 w-full min-h-[100px]'>
       {technologies.map((technology) => (
-        <div className='w-28 h-28' key={technology.name}>
+        <div
+          className='w-28 h-28 flex items-center justify-center flex-shrink-0'
+          key={technology.name}>
           <BallCanvas icon={technology.icon} />
         </div>
       ))}
